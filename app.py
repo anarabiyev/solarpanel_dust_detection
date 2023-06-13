bc0818ecebee0b17483fed20d709e75196c22335
anarabiyev <50334081+anarabiyev@users.noreply.github.com>
2023-06-13 08:06:31 +0400

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from PIL import Image
 import os
 import pickle
 import numpy as np
-from sklearn.ensemble import RandomForestClassifier
+#from sklearn.ensemble import RandomForestClassifier
 from sklearn.datasets import make_classification
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
